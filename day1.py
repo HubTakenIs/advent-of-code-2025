d8463900d4f2f1a16fd02e2448297b37618091b8
HubTakenIs <hubertaslol@gmail.com>
2025-12-02 16:04:38 +0000

    day1part2 complete.

--- day1.py
+++ day1.py
@@ -2,60 +2,47 @@ class Dial:
     def __init__(self):
         self.position = 50
         self.zeroes = 0
-    
-    def process_line(self,line):
+
+    def process_line(self, line):
         direction = line[0]
         distance = int(line[1:])
-        return direction,distance
-    def turn_dial(self, line):
-        print(f"current dial position: {self.position}")
-        direction, distance = self.process_line(line)
-        print(f"turning {direction}, for {distance} click(s)")
-       
-        div = divmod(distance,100)
+        return direction, distance
 
-        if div[0] > 0:
-            distance = div[1]
+    def turn_dial(self, line):
+        direction, distance = self.process_line(line)
+        div = divmod(distance, 100)
+        new_pos = 0
+        # print(f"div: 0| {div[0]} : 1|{div[1]}")
 
         if direction == "L":
-            temp = self.position - distance
-            if temp < 0:
-                self.position = 100 + temp
-            if temp == 0:
-                self.position = temp
-                self.zeroes +=1
-            if temp > 0:
-                self.position = temp
+            new_pos = (self.position - div[1]) % 100
+            if self.position - div[1] <= 0 and self.position != 0:
+                # rolled.
+                # print("counted roll for L")
+                self.zeroes += 1
 
         if direction == "R":
-            temp = self.position + distance
-            if temp > 99:
-                self.position = 0 + (temp-100)
-            if temp <= 99:
-                self.position = temp
-            if self.position == 0:
-                self.zeroes +=1
-        print(f"Turned Dial to position {self.position}")
+            new_pos = (self.position + div[1]) % 100
+            if self.position + div[1] >= 100:
+                # rolled
+                # print("counted roll for R")
+                self.zeroes += 1
+
+        if div[0] > 0:
+            self.zeroes += div[0]
+
+        self.position = new_pos
+        # print("new pos: ", new_pos)
+
+
 def main():
     with open("day1_input") as f:
         lines = f.read().splitlines()
-    print(lines)
-    print(f"read lines: {len(lines)}")
-    print(f"line to be processed: {lines[0]}")
-
     dial = Dial()
-    # that's bad. dial doesn't need to contain process_lines.
     for line in lines:
         dial.turn_dial(line)
     print(f"password should be: {dial.zeroes}")
-    # I need to track of how many times the dial has hit 0.
-    # initial position is 50.
-    # 
-    
 
 
 if __name__ == "__main__":
     main()
-
-
-
--- test_day1.py
+++ test_day1.py
@@ -1,39 +1,96 @@
 import unittest
+from modulefinder import test
+
 from day1 import Dial
 
+
 class Day1Tests(unittest.TestCase):
-    
     def test_provided_example(self):
         dial = Dial()
-        test_lines =["L68","L30", "R48", 
-                     "L5",
-                     "R60",
-                     "L55",
-                     "L1",
-                     "L99",
-                     "R14",
-                     "L82"]
+        test_lines = [
+            "L68",
+            "L30",
+            "R48",
+            "L5",
+            "R60",
+            "L55",
+            "L1",
+            "L99",
+            "R14",
+            "L82",
+        ]
         for line in test_lines:
-            print(f"test_line {line}")
             dial.turn_dial(line)
 
-        self.assertEqual(3,dial.zeroes)
-    
-    def test_big_numbers1(self):
+        self.assertEqual(6, dial.zeroes)
+
+    def test_return1_1(self):
         dial = Dial()
-        test_lines = [
-                "R66",
-                "L2",
-                "L345",
-                "R6",
-                "L61",
-                "R84",
-                "R2"
-                ]
+        test_lines = ["L50", "R50"]
+
         for line in test_lines:
-            print(f"test_line: {line}")
+            dial.turn_dial(line)
+
+        self.assertEqual(1, dial.zeroes)
+
+    def test_return1_2(self):
+        dial = Dial()
+        test_lines = ["L50", "L50"]
+
+        for line in test_lines:
+            dial.turn_dial(line)
+
+        self.assertEqual(1, dial.zeroes)
+
+    def test_return1_3(self):
+        dial = Dial()
+        test_lines = ["R50", "L50"]
+
+        for line in test_lines:
+            dial.turn_dial(line)
+
+        self.assertEqual(1, dial.zeroes)
+
+    def test_return1_4(self):
+        dial = Dial()
+        test_lines = ["R50", "R50"]
+
+        for line in test_lines:
             dial.turn_dial(line)
         self.assertEqual(1, dial.zeroes)
+
+    def test_return2_1(self):
+        dial = Dial()
+        test_lines = ["L150", "L50"]
+
+        for line in test_lines:
+            dial.turn_dial(line)
+        self.assertEqual(2, dial.zeroes)
+
+    def test_return2_2(self):
+        dial = Dial()
+        test_lines = ["L150", "R50"]
+
+        for line in test_lines:
+            dial.turn_dial(line)
+        self.assertEqual(2, dial.zeroes)
+
+    def test_return2_3(self):
+        dial = Dial()
+        test_lines = ["R150", "L50"]
+
+        for line in test_lines:
+            dial.turn_dial(line)
+        self.assertEqual(2, dial.zeroes)
+
+    def test_return2_4(self):
+        dial = Dial()
+        test_lines = ["R150", "R50"]
+
+        for line in test_lines:
+            dial.turn_dial(line)
+        self.assertEqual(2, dial.zeroes)
+
 
 if __name__ == "__main__":
     unittest.main()
